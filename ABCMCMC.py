931c2eba663eea1968edf3b6708a23e124785622
Zarebski <Alex@Alex-Zarebskis-MacBook-Pro.local>
2015-05-16 13:33:52 +1000

    finish the code. write some main() code to plot a pretty representation of the simulation results and the true posterior.
    takes about a minute and a half to run this as is.

--- ABCMCMC.py
+++ ABCMCMC.py
@@ -17,27 +17,31 @@ def q_sample(theta):
 	"""
 	r = 2
 	while r >= 1 or r <= 0:
-		r = np.random.normal(loc=theta, scale=0.1)
+		r = np.random.normal(loc=theta, scale=0.1) 	# scale = 0.1
 	return r
 
 def q(theta_prop,theta_current):
 	"""returns the value of the proposal density at theta_prop conditioned on 
 	the current postion theta_current
-						IN PROGRESS
+						FINISHED!!!
 	"""
-	return None
+	n = ss.norm(loc=theta_current, scale=0.1) 	# scale = 0.1 as in q_sample
+	numerator = n.pdf(theta_prop)
+	denominator = n.cdf(1) - n.cdf(0)
+	return numerator/denominator
 
-def f(theta):
+def f(theta, num_flips):
 	"""returns a data set generated with the parameter theta
-						IN PROGRESS
+						FINISHED!!!
 	"""
-	return None
+	result = np.random.binomial(n=num_flips, p=theta)
+	return result
 
 def d(X_true,X_star):
 	"""returns the distance between samples via a metric on a summary statistic
-						IN PROGRESS
+						FINISHED!!!
 	"""
-	return None
+	return abs(X_true - X_star) 
 
 def prior(theta):
 	"""returns the value of the prior distribution evaluated at theta
@@ -49,8 +53,17 @@ def prior(theta):
 		y = 0
 	return y
 
+def true_posterior(mesh, X_true, num_flips):
+	"""returns the value of the true posterior evaluated on the mesh
+						FINISHED!!!
+	"""
+	a = X_true + 1
+	b = num_flips - X_true + 1
+	result = ss.beta.pdf(mesh,a,b)
+	return result
 
-def ABC_MCMC(epsilon, N, X_true):
+
+def ABC_MCMC(epsilon, N, X_true, num_flips):
 	"""Carries out a MCMC of N steps and returns the states visited
 						FINISHED!!!
 	"""
@@ -61,23 +74,39 @@ def ABC_MCMC(epsilon, N, X_true):
 	#-------------------------- M2 --------------------------#
 	while i < N:
 		theta_star = q_sample(theta)
-	    #---------------------- M3 --------------------------#
-	    X_star = f(theta_star)
-	    #---------------------- M4 --------------------------#
-	    if d(X_true,X_star) <= epsilon:
-	    	#------------------ M5 --------------------------#
-	    	foo = ( prior(theta_star)*q(theta,theta_star) )/\
-	    		( prior(theta)*q(theta_star,theta) )
-	    	alpha = min(1, foo)
-	    	r = np.random.uniform()
-	    	if r <= alpha:
-	    		theta = theta_star
-	    theta_chain += [theta]
-	    #---------------------- M6 --------------------------#
-	    i += 1
+		#---------------------- M3 --------------------------#
+		X_star = f(theta_star, num_flips)
+		#---------------------- M4 --------------------------#
+		if d(X_true,X_star) <= epsilon:
+			#------------------ M5 --------------------------#
+			foo = ( prior(theta_star)*q(theta,theta_star) )/( prior(theta)*q(theta_star,theta) )
+			alpha = min(1, foo)
+			r = np.random.uniform()
+			if r <= alpha:
+				theta = theta_star
+		theta_chain += [theta]
+		#---------------------- M6 --------------------------#
+		i += 1
 	return theta_chain
 
 def main():
+	epsilon = 1			# tolerance
+	N = 10**5			# number of states to sample from chain
+	X_true = 5			# observed number of heads
+	num_flips = 10		# number of coin flips
+
+	chain = ABC_MCMC(epsilon, N, X_true, num_flips)
+
+	xx = np.linspace(0,1,100)
+	yy = true_posterior(xx,X_true,num_flips)
+	plt.plot(xx,yy,'r--')
+
+	plt.hist(chain, bins=20, normed=True)
+	plt.title("Chain Histogram")
+	plt.xlabel("Value")
+	plt.ylabel("Frequency")
+	plt.show()
+
 	return None
 
 if __name__ == '__main__':
